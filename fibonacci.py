3c8ad1fc92afd1919338faaacb3ba28e218d9476
David Rees <astroponyrees@gmail.com>
2023-08-19 07:07:59 -0600

    Implemented Recursive Fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+def fibonacci(num):
+    if num <= 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        return fibonacci(num-1) + fibonacci(num-2)
+    
+for i in range(0,11):
+    print(fibonacci(i))
